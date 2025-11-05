d8eff86f91aa6fed57b7570b719a5aefa7e885f8
Leniors <mokualeeroy@gmail.com>
2025-11-05 21:40:44 +0300

    rently

--- integrations/supabase/client.ts
+++ integrations/supabase/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "ostolcpvctebgvhqlpuq";
+const SUPABASE_URL = "https://ostolcpvctebgvhqlpuq.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdG9sY3B2Y3RlYmd2aHFscHVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNTEwNzEsImV4cCI6MjA3NDgyNzA3MX0.lsTTv7TuubwJY7wHQvCKtHVKpTrfLjWfedEeGlJsAr0";
 
 // Import the supabase client like this:
